f37778967b6c7edcae1680987c79ee7fd25e8949
domsson <julien.d.dau@gmail.com>
2024-04-21 15:56:16 +0000

    make item API cache its results

--- api/item.php
+++ api/item.php
@@ -4,6 +4,8 @@ require_once "../lib/yon_core.php";
 require_once "../lib/yon_json.php";
 require_once "../lib/yon_request.php";
 
+define("MARKET_DIR", "../cache/market");
+
 $item_id = yon_get_http_var("item_id", FILTER_VALIDATE_INT);
 $api_key = yon_get_http_var("api_key");
 
@@ -13,7 +15,24 @@ $api_data = [
 	"key" => $api_key 
 ];
 
-$result = yon_http_get($api_url, $api_data);
-yon_echo_and_exit($result, true, true);
+$filepath = MARKET_DIR . "/${item_id}.json";
+$data = null;
+
+if (file_exists($filepath))
+{
+	$age = time() - filemtime($filepath);
+	if ($age < 2)
+	{
+		$data = yon_json_file_load($filepath);
+	}
+}
+
+if (empty($data))
+{
+	$data = yon_http_get($api_url, $api_data);
+	yon_json_file_save($filepath, $data);
+}
+
+yon_echo_and_exit($data, true, true);
 
 ?>
